364277531a5b6f8970f68175c63b999c21f6a5d2
cyber-mat <olaniyanolamide@gmail.com>
2020-07-18 18:17:00 +0100

    New game button

--- js/script.js
+++ js/script.js
@@ -13,33 +13,44 @@ const gameController = (function () {
     },
 
     checkWinner(gamePlaying) {
+      let winner;
       if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) {
         gamePlaying = false;
+        winner = gameBoard[0];
       } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
         gamePlaying = false;
+        winner = gameBoard[0];
       } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
         gamePlaying = false;
+        winner = gameBoard[0];
       } else if (gameBoard[1] !== '' && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
         gamePlaying = false;
+        winner = gameBoard[1];
       } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) {
         gamePlaying = false;
+        winner = gameBoard[2];
       } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
         gamePlaying = false;
+        winner = gameBoard[2];
       } else if (gameBoard[3] !== '' && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
         gamePlaying = false;
+        winner = gameBoard[3];
       } else if (gameBoard[6] !== '' && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
         gamePlaying = false;
+        winner = gameBoard[6];
       } else if (!gameBoard.includes('')) {
         gamePlaying = false;
+        winner = '';
       } else {
         gamePlaying = true;
       }
 
-      return gamePlaying;
+      return { winner, gamePlaying };
     },
 
     resetBoard() {
       gameBoard = ['', '', '', '', '', '', '', '', ''];
+      return gameBoard;
     },
 
     test() {
@@ -68,12 +79,12 @@ const displayController = (function () {
     },
 
     changePlayer(marker) {
-      if (currentPlayer === 'X') {
-        currentPlayer = 'O';
+      if (marker === 'X') {
+        marker = 'O';
       } else {
-        currentPlayer = 'X';
+        marker = 'X';
       }
-      return currentPlayer;
+      return marker;
     },
 
     renderCells(board, cell, id) {
@@ -84,6 +95,10 @@ const displayController = (function () {
       const cellArr = Array.from(dom.cell);
       cellArr.forEach((el) => (el.textContent = ''));
     },
+
+    togglePlayer() {
+      dom.notif.textContent === 'Player 1' ? (dom.notif.textContent = 'Player 2') : (dom.notif.textContent = 'Player 1');
+    },
   };
 })();
 
@@ -92,14 +107,15 @@ OVERALL APP CONTROLLER
 */
 
 const appController = (function (gameCtrl, displayCtrl) {
-  let gamePlaying;
+  let board, marker, gamePlaying, winner;
   const dom = displayCtrl.dom;
+  board = gameCtrl.gameBoard;
+  marker = 'O';
 
   dom.board.addEventListener('click', (e) => {
     if (gamePlaying) {
       if (e.target.matches('.cell')) {
         const cell = e.target;
-        const board = gameCtrl.gameBoard;
         const id = parseInt(e.target.dataset.id);
         //console.log(cell);
 
@@ -107,7 +123,7 @@ const appController = (function (gameCtrl, displayCtrl) {
         //const marker = displayCtrl.getMarker();
 
         // Change Player
-        const marker = displayCtrl.changePlayer();
+        marker = displayCtrl.changePlayer(marker);
 
         // Store marker in clicked position in data structure
         gameCtrl.storeMarker(id, marker);
@@ -115,8 +131,12 @@ const appController = (function (gameCtrl, displayCtrl) {
         // Render cells on board
         displayCtrl.renderCells(board, cell, id);
 
+        // Toggle notification panel
+        displayCtrl.togglePlayer();
+
         // Check if game has been won or tied
-        gamePlaying = gameCtrl.checkWinner(gamePlaying);
+        gamePlaying = gameCtrl.checkWinner(gamePlaying).gamePlaying;
+        winner = gameCtrl.checkWinner(gamePlaying).winner;
 
         if (!gamePlaying) {
           gameCtrl.resetBoard();
@@ -128,11 +148,12 @@ const appController = (function (gameCtrl, displayCtrl) {
 
   // Event listener for new game button
   dom.newBtn.addEventListener('click', () => {
-    gameCtrl.resetBoard();
+    board = gameCtrl.resetBoard();
     displayCtrl.clearBoard();
+    marker = 'O';
 
     gamePlaying = true;
-    dom.notif.textContent = 'Player ...';
+    dom.notif.textContent = 'Player 1';
   });
 
   // Event listener for window object(page load)
@@ -140,7 +161,7 @@ const appController = (function (gameCtrl, displayCtrl) {
     gamePlaying = true;
     displayCtrl.clearBoard();
 
-    dom.notif.textContent = 'Player ...';
+    dom.notif.textContent = 'Player 1';
   });
 })(gameController, displayController);
 
