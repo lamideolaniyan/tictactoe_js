80949b90749356dee3d6c0337788e5113ad6be93
cyber-mat <olaniyanolamide@gmail.com>
2020-07-18 14:47:49 +0100

    Basic functionality

--- js/script.js
+++ js/script.js
@@ -3,12 +3,46 @@ GAME CONTROLLER
 */
 
 const gameController = (function () {
-  const gameBoard = ['', '', '', '', '', '', '', '', ''];
+  let gameBoard = ['', '', '', '', '', '', '', '', ''];
 
   return {
     gameBoard,
     storeMarker(id, marker) {
       gameBoard[id] = marker;
+      console.log(gameBoard);
+    },
+
+    checkWinner(gamePlaying) {
+      if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) {
+        gamePlaying = false;
+      } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
+        gamePlaying = false;
+      } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
+        gamePlaying = false;
+      } else if (gameBoard[1] !== '' && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
+        gamePlaying = false;
+      } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) {
+        gamePlaying = false;
+      } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
+        gamePlaying = false;
+      } else if (gameBoard[3] !== '' && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
+        gamePlaying = false;
+      } else if (gameBoard[6] !== '' && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
+        gamePlaying = false;
+      } else if (!gameBoard.includes('')) {
+        gamePlaying = false;
+      } else {
+        gamePlaying = true;
+      }
+
+      return gamePlaying;
+    },
+
+    resetBoard() {
+      gameBoard = ['', '', '', '', '', '', '', '', ''];
+    },
+
+    test() {
       console.log(gameBoard);
     },
   };
@@ -43,6 +77,11 @@ const displayController = (function () {
     renderCells(board, cell, id) {
       cell.textContent = board[id];
     },
+
+    clearBoard() {
+      const cellArr = Array.from(dom.cell);
+      cellArr.forEach((el) => (el.textContent = ''));
+    },
   };
 })();
 
@@ -51,33 +90,43 @@ OVERALL APP CONTROLLER
 */
 
 const appController = (function (gameCtrl, displayCtrl) {
-  let cellNum;
+  let gamePlaying;
   const dom = displayCtrl.dom;
 
   dom.board.addEventListener('click', (e) => {
-    if (e.target.matches('.cell')) {
-      const cell = e.target;
-      const board = gameCtrl.gameBoard;
-      const id = parseInt(e.target.dataset.id);
-      //console.log(cell);
+    if (gamePlaying) {
+      if (e.target.matches('.cell')) {
+        const cell = e.target;
+        const board = gameCtrl.gameBoard;
+        const id = parseInt(e.target.dataset.id);
+        //console.log(cell);
 
-      // Get marker
-      //const marker = displayCtrl.getMarker();
+        // Get marker
+        //const marker = displayCtrl.getMarker();
 
-      // Change Player
-      const marker = displayCtrl.changePlayer();
+        // Change Player
+        const marker = displayCtrl.changePlayer();
 
-      // Store marker in clicked position in data structure
-      gameCtrl.storeMarker(id, marker);
+        // Store marker in clicked position in data structure
+        gameCtrl.storeMarker(id, marker);
 
-      // Render cells on board
-      displayCtrl.renderCells(board, cell, id);
+        // Render cells on board
+        displayCtrl.renderCells(board, cell, id);
+
+        // Check if game has been won or tied
+        gamePlaying = gameCtrl.checkWinner(gamePlaying);
+
+        if (!gamePlaying) {
+          gameCtrl.resetBoard();
+          alert('Gameover!');
+        }
+      }
     }
   });
 
   window.addEventListener('load', (e) => {
-    const cellArr = Array.from(dom.cell);
-    cellArr.forEach((el) => (el.textContent = ''));
+    gamePlaying = true;
+    displayCtrl.clearBoard();
   });
 })(gameController, displayController);
 
